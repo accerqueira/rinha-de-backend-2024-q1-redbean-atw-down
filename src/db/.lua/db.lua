4d4c082e5085272b8784d8a362ec282e95a1aaab
Andre Cerqueira <me@andrecerqueira.com>
2024-02-19 10:29:23 -0300

    feat: db cache

--- src/db/.lua/db.lua
+++ src/db/.lua/db.lua
@@ -1,3 +1,4 @@
+-- local dump = require "dump"
 local mod = {}
 
 function mod.open(path, script)
@@ -38,7 +39,45 @@ function mod.open(path, script)
       RETURNING *;
   ]]
 
+  local function fetch(query)
+    local result = {}
+    for row in db:nrows(query) do
+      table.insert(result, row)
+    end
+    return result
+  end
+
+  local cache = {
+    clients = fetch("SELECT * FROM clients"),
+    transactions = fetch("SELECT * FROM transactions"),
+  }
+  cache["clients_by_id"] = {}
+  for idx, client in ipairs(cache.clients) do
+    cache["clients_by_id"][client.id] = idx
+  end
+  cache["transactions_by_client"] = {}
+  cache["transactions_by_client"] = {}
+  for _, client in ipairs(cache.clients) do
+    cache["transactions_by_client"][client.id] = {}
+  end
+  for idx, transaction in ipairs(cache.transactions) do
+    local client_id = transaction.client_id
+    table.insert(cache["transactions_by_client"][client_id], idx)
+  end
+
   local function select_client_with_timestamp(params)
+    -- { client_id = client_id }
+    local client_id = tonumber(params.client_id)
+    if not (client_id >= 1 and client_id <= 5) then
+      return nil
+    end
+
+    local client_idx = cache["clients_by_id"][client_id]
+    cache.clients[client_idx].timestamp = os.date("!%Y-%m-%dT%TZ")
+    return cache.clients[client_idx]
+  end
+
+  local function select_client_with_timestamp_slow(params)
     -- { client_id = client_id }
     local stmt = sql_select_client_with_timestamp_stmt
     stmt:bind_names(params)
@@ -50,10 +89,17 @@ function mod.open(path, script)
 
     local r = stmt:get_named_values()
     stmt:reset()
+
     return r
   end
 
   local function select_client_exists(params)
+    -- { client_id = client_id }
+    local client_id = tonumber(params.client_id)
+    return client_id >= 1 and client_id <= 5
+  end
+
+  local function select_client_exists_slow(params)
     -- { client_id = client_id }
     local stmt = sql_select_client_exists_stmt
     stmt:bind_names(params)
@@ -75,6 +121,11 @@ function mod.open(path, script)
 
     local r = stmt:get_named_values()
     stmt:reset()
+
+    local client_id = tonumber(params.client_id)
+    local client_idx = cache["clients_by_id"][client_id]
+    cache.clients[client_idx].balance = cache.clients[client_idx].balance + params.value
+
     return r
   end
 
@@ -85,10 +136,37 @@ function mod.open(path, script)
 
     local r = stmt:step()
     stmt:reset()
+
+    params["performed_at"] = os.date("!%Y-%m-%dT%TZ")
+    table.insert(cache.transactions, 1, params)
+
+    local client_id = tonumber(params.client_id)
+    table.insert(cache["transactions_by_client"][client_id], #cache.transactions)
+    if #cache["transactions_by_client"][client_id] > 10 then
+      local idx = table.remove(cache["transactions_by_client"][client_id], 11)
+      if idx ~= nil then
+        table.remove(cache.transactions, idx)
+      end
+    end
+
     return r == sqlite3.DONE
   end
 
   local function select_transactions_lastest10(params)
+    -- { client_id = client_id }
+    local result = {}
+    for _, transaction in ipairs(cache.transactions) do
+      if transaction.client_id == params.client_id then
+        table.insert(result, transaction)
+        if #result >= 10 then
+          break
+        end
+      end
+    end
+    return result
+  end
+
+  local function select_transactions_lastest10_slow(params)
     -- { client_id = client_id }
     local stmt = sql_select_transactions_lastest10_stmt
     stmt:bind_names(params)
@@ -102,7 +180,6 @@ function mod.open(path, script)
   end
 
   return {
-    executeScript = executeScript,
     select_client_exists = select_client_exists,
     select_client_with_timestamp = select_client_with_timestamp,
     update_client_balance = update_client_balance,
