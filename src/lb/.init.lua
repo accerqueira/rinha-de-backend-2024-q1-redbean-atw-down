2766dcce1ea7020065a532bc9f552dd51e8ff838
Andre Cerqueira <me@andrecerqueira.com>
2024-02-19 13:40:45 -0300

    feat: lb local request counter (single-threaded only)

--- src/lb/.init.lua
+++ src/lb/.init.lua
@@ -1,16 +1,15 @@
 local env = require "env"
 local assign = require "assign"
 
-local db = require "db"
-local repo = db.open("db.sqlite3")
-
 local servers = {}
 for server in string.gmatch(env["SERVERS"], "[^|]+") do
   table.insert(servers, server)
 end
 
+local state = { request_counter = 0 }
+
 function OnHttpRequest()
-  local state = repo.get_updated_state() or { request_counter = math.random(1, #servers) }
+  state.request_counter = (state.request_counter + 1) % 128
 
   local idx = (state.request_counter % #servers) + 1
   local url = servers[idx] .. EscapePath(GetPath())
